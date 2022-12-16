3094e140d304aceb964bc7fcddce07e881aa62b0
esrb2000 <120108344+esrb2000@users.noreply.github.com>
2022-12-16 13:36:50 +0700

    Add files via upload
    initial

---
+++ Elevator.java
@@ -0,0 +1,40 @@
+public class Elevator {
+    private int currentFloor = 1;
+    private int minFloor;
+    private int maxFloor;
+
+    public Elevator (int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+    public int moveUp() {
+        currentFloor = currentFloor + 1;
+        return currentFloor;
+    }
+    public void moveDown() {
+        currentFloor = currentFloor - 1;
+    }
+    public void move(int floor) {
+        if ((floor < maxFloor) && (floor > minFloor)) {
+            if (currentFloor < floor) {
+                for (; currentFloor < floor; ) {
+                    moveUp();
+                    System.out.println(getCurrentFloor());
+                }
+            } else if (currentFloor > floor) {
+                for (; currentFloor > floor; ) {
+                    moveDown();
+                    System.out.println(getCurrentFloor());
+                }
+            } else {
+                System.out.println(currentFloor);
+            }
+        } else {
+            System.out.println("Введен некоректный этаж!");
+        }
+    }
+}
+
