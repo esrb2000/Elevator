a78418f25303ffd0cdaf0f81694fae1f5cad870f
Boris Kuptsevich <esrb2000@bk.ru>
2022-12-20 21:25:46 +0700

    add class cargo

---
+++ Cargo.java
@@ -0,0 +1,12 @@
+public class Cargo {
+    private final double width;
+    private final double length;
+    private final double heigth;
+    private final double weigth;
+    private final String diveleryAdress;
+    private final boolean flip;
+    private final String registrationNumber;
+    private final boolean fragile;
+
+
+}
